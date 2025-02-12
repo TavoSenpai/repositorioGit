f19597ea330ce4a26fce1e5a961bab832b64dec7
TavoSenpai <tavodelasal@gmail.com>
2025-02-11 18:50:54 -0600

    Se sube respositorio

---
+++ src/main/java/com/mycompany/repositoriogit/RepositorioGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositoriogit;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class RepositorioGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
