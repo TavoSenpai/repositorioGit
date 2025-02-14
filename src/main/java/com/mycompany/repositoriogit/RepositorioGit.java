b780254746e1559773ac1f263a945f33efeadaca
TavoSenpai <tavodelasal@gmail.com>
2025-02-13 18:03:09 -0600

    Se actualizó

--- src/main/java/com/mycompany/repositoriogit/RepositorioGit.java
+++ src/main/java/com/mycompany/repositoriogit/RepositorioGit.java
@@ -11,6 +11,7 @@ package com.mycompany.repositoriogit;
 public class RepositorioGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola");
+        
     }
 }
